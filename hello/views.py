729186466f28174ce0e6e339befefa4b6da5727e
watanabe-atrasc <watanabesa@atrasc.co.jp>
2019-02-22 10:30:48 +0900

    commit

--- hello/views.py
+++ hello/views.py
@@ -1,20 +1,10 @@
-from django.shortcuts import render
 from django.http import HttpResponse
-from .models import Friend
-from .forms import HelloForm
+from .models import WorkData
 
 def index(request):
-    params = {
-            'title': 'Hello',
-            'message': 'all friends.',
-            'form':HelloForm(),
-            'data': [],
-        }
-    if (request.method == 'POST'):
-        num=request.POST['id']
-        item = Friend.objects.get(id=num)
-        params['data'] = [item]
-        params['form'] = HelloForm(request.POST)
-    else:
-        params['data'] = Friend.objects.all()
-    return render(request, 'hello/index.html', params)
+    
+    data = []    
+    data = WorkData.objects.all().values()
+    return HttpResponse(data)
+
+    
