1d7916fb274e0d5d531100c3af389f2ca67f9ce9
watanabe-atrasc <watanabesa@atrasc.co.jp>
2019-03-07 15:41:51 +0900

    commit20190307

--- django_app/settings.py
+++ django_app/settings.py
@@ -74,6 +74,7 @@ WSGI_APPLICATION = 'django_app.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
+"""
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
@@ -87,7 +88,13 @@ DATABASES = {
         }
     }
 }
-
+"""
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/2.1/ref/settings/#auth-password-validators
