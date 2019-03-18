f435a62f0f51447eb81e66d8fd72b85ca16b14c9
watanabe-atrasc <watanabesa@atrasc.co.jp>
2019-03-18 13:04:53 +0900

    commit

--- django_app/settings.py
+++ django_app/settings.py
@@ -23,11 +23,11 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '*3x1-!tml4503*=9e!$wk%0+a1e60b4f3^#+@b010_wbk3nv1m'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-#ALLOWED_HOSTS = ['*', 'https://pythonap.azurewebsites.net']
+ALLOWED_HOSTS = ['*', 'https://pythonap.azurewebsites.net']
 #ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
-ALLOWED_HOSTS = ['*']
+#ALLOWED_HOSTS = ['*']
 
 # Application definition
 
